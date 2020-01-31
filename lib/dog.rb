290a68df15adbe42422dc0095c02ebf73c359915
Micah Banschick <micha.ban@gmail.com>
2020-01-31 20:21:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ class Dog
   
   def initialize(name)
     @name = name 
-    @@all << self 
+    @save 
   end 
   
   def self.all 
@@ -23,7 +23,7 @@ class Dog
   end 
   
   def save
-    @@all << self if @@all.include?(self) == false 
+    @@all << self
   end 
   
 end
